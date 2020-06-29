d6a705c7273f54467906676fc2c7297a232bd332
Yoshino Takazawa <yochiyochiperry2000@gmail.com>
2020-06-29 10:00:17 +0000

    CategoryのAPIを作成した

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,43 @@
 class CategoriesController < ApplicationController
+    before_action :set_category, only: %i[show update destroy]
+  def index
+    @categoris = Category.all
+    render json: @categoris
+  end
+
+  def show
+    render json: @category
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      render json: @category, status: :created, location: @category
+    else
+      render json: @category.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @category.update(category_params)
+      render json: @category
+    else
+      render json: @category.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @category.destroy
+  end
+
+  private
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
 end
